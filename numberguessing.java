3ead83896652749a9e7e5bd1f835368d824a1da2
Deepak <deepakcm27@gmail.com>
2025-07-14 23:47:03 +0530

    Update numberguessing.java

--- numberguessing.java
+++ numberguessing.java
@@ -4,7 +4,7 @@ class numberguessing{
     public static void main(String[] args) {
     System.out.println("Welcome to the Number guessing game");
     System.out.println("Rules: 1. Enter a number from 1 to 20");
-    system.out.println("2.If it is right to my guessed number then You are WINNER");
+    System.out.println("2.If it is right to my guessed number then You are WINNER");
     System.err.println("3.Also I Will Give You Some Hints(Whether it is too high or too low), But you have only 5 CHANCES!!!");
     int chances=5;
     Random randnum=new Random();
